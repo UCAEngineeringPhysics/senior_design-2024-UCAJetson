7076e2611caf8084da04a2c0c0f318d90fda4fb5
ashton <ashtonmjohns@gmail.com>
2024-09-29 15:17:46 -0500

    update to reflect correct pins

--- scripts/pico/main.py
+++ scripts/pico/main.py
@@ -8,7 +8,7 @@ from time import sleep
 from machine import Pin, PWM
 
 # SETUP
-steering = PWM(Pin(0))
+steering = PWM(Pin(16))
 steering.freq(50)
 throttle = PWM(Pin(15))
 throttle.freq(50)
@@ -18,11 +18,22 @@ poller.register(sys.stdin, select.POLLIN)
 event = poller.poll()
 
 # LOOP
-while True:
-    # read data from serial
-    for msg, _ in event:
-        buffer = msg.readline().rstrip().split(',')
-        if len(buffer) == 2:
-            ns_st, ns_th = int(buffer[0]), int(buffer[1])
-            steering.duty_ns(ns_st)
-            throttle.duty_ns(ns_th)
+try:
+    while True:
+        # read data from serial
+        for msg, _ in event:
+            buffer = msg.readline().rstrip().split(',')
+            if len(buffer) == 2:
+                ns_st, ns_th = int(buffer[0]), int(buffer[1])
+                print(f"steering: {ns_st}\nthrottle: {ns_th}")
+                steering.duty_ns(ns_st)
+                throttle.duty_ns(ns_th)
+except:
+    pass
+finally:
+    throttle.duty_ns(1210000)
+    sleep(1)
+    throttle.deinit()
+    steering.duty_ns(1500000)
+    sleep(1)
+    steering.deinit()
