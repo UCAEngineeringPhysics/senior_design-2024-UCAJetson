3b41eccc38455eb2bb7dd163055e5dae4c2014c6
ashton <ashtonmjohns@gmail.com>
2024-09-29 15:17:30 -0500

    testing

--- scripts/pico/esc_throttle_test.py
+++ scripts/pico/esc_throttle_test.py
@@ -1,30 +1,42 @@
-from machine import Pin, PWM
+from machine import Pin, PWM, reset
 from time import sleep
 
 # SETUP
 motor = PWM(Pin(15))
-motor.freq(50)
+motor.freq(10000)
 sleep(3)
 
 # LOOP
-for i in range(1210000, 1800000, 10000): # forward up
-    motor.duty_ns(i)
-    print(i)
-    sleep(0.2)
-for i in reversed(range(1210000, 1800000, 10000)): # forward down
-    motor.duty_ns(i)
-    print(i)
-    sleep(0.2)
-for i in reversed(range(1090000, 1210000, 10000)): # reverse up
-    motor.duty_ns(i)
-    print(i)
-    sleep(0.2)
-for i in range(1090000, 1210000, 10000): # reverse down
-    motor.duty_ns(i)
-    print(i)
-    sleep(0.2)
-motor.duty_ns(1210000)
-sleep(1)
-motor.deinit()
+max = 65535
+try:
+    for i in range(0,max,1500):
+        motor.duty_u16(i)
+        print("Waiting 1 second...")
+        sleep(0.2)
+    # for i in range(1210000, 1800000, 5000): # forward up
+    #     motor.duty_ns(i)
+    #     print(i)
+    #     sleep(0.2)
+    # for i in reversed(range(1210000, 1800000, 5000)): # forward down
+    #     motor.duty_ns(i)
+    #     print(i)
+    #     sleep(0.2)
+    # for i in reversed(range(1090000, 1210000, 10000)): # reverse up
+    #     motor.duty_ns(i)
+    #     print(i)
+    #     sleep(0.5)
+    # for i in range(1090000, 1210000, 10000): # reverse down
+    #     motor.duty_ns(i)
+    #     print(i)
+    #     sleep(0.5)
+except:
+    print("Exception")
+    pass
+finally:
+    motor.duty_ns(0)
+    sleep(1)
+    motor.deinit()
+    # reset()
+    
 
 
