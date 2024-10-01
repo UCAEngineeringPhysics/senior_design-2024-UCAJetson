b995b6c91ec622a7b73c6b48f45bdf9b77292e8c
ashton <ashtonmjohns@gmail.com>
2024-10-01 17:50:48 -0500

    working on ranges

--- scripts/pico/esc_servo_test.py
+++ scripts/pico/esc_servo_test.py
@@ -6,14 +6,20 @@ servo = PWM(Pin(16))
 servo.freq(50)
 
 # LOOP
-for i in range(1000000, 2000000, 10000):
-    servo.duty_ns(i)
-    print(i)
-    sleep(0.2)
-for i in reversed(range(1000000, 2000000, 10000)):
-    servo.duty_ns(i)
-    print(i)
-    sleep(0.2)
-servo.duty_ns(1500000)
-servo.deinit()
+try:
+    print("Turning Right")
+    for i in range(15000, 20000, 100): 
+        servo.duty_ns(i)
+        print(i)
+        sleep(0.2)
+    print("Turning Left")
+    for i in reversed(range(1500000, 2000000, 10000)): 
+        servo.duty_ns(i)
+        print(i)
+        sleep(0.2)
+except:
+    print("Exception")
+finally:
+    servo.duty_ns(0)
+    servo.deinit()
 
--- scripts/pico/esc_throttle_test.py
+++ scripts/pico/esc_throttle_test.py
@@ -3,35 +3,33 @@ from time import sleep
 
 # SETUP
 motor = PWM(Pin(15))
-motor.freq(10000)
+motor.freq(50)
 sleep(3)
 
 # LOOP
-max = 65535
 try:
-    for i in range(0,max,1500):
-        motor.duty_u16(i)
-        print("Waiting 1 second...")
+    print("Speeding up going forward")
+    for i in range(1500000, 2000000, 5000): # forward up
+        motor.duty_ns(i)
+        print(i)
         sleep(0.2)
-    # for i in range(1210000, 1800000, 5000): # forward up
-    #     motor.duty_ns(i)
-    #     print(i)
-    #     sleep(0.2)
-    # for i in reversed(range(1210000, 1800000, 5000)): # forward down
-    #     motor.duty_ns(i)
-    #     print(i)
-    #     sleep(0.2)
-    # for i in reversed(range(1090000, 1210000, 10000)): # reverse up
-    #     motor.duty_ns(i)
-    #     print(i)
-    #     sleep(0.5)
-    # for i in range(1090000, 1210000, 10000): # reverse down
-    #     motor.duty_ns(i)
-    #     print(i)
-    #     sleep(0.5)
+    print("Slowing down")
+    for i in reversed(range(1500000, 2000000, 5000)): # forward down
+        motor.duty_ns(i)
+        print(i)
+        sleep(0.2)
+    print("Speeding up going backward")
+    for i in reversed(range(1200000, 1500000, 5000)): # forward up
+        motor.duty_ns(i)
+        print(i)
+        sleep(0.2)
+    print("Slowing down")
+    for i in range(1200000, 1500000, 5000): # forward down
+        motor.duty_ns(i)
+        print(i)
+        sleep(0.2)
 except:
     print("Exception")
-    pass
 finally:
     motor.duty_ns(0)
     sleep(1)
