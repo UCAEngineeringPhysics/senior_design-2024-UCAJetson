2187869156be3edc6ea45207e64469222a8b6769
ashton <ashtonmjohns@gmail.com>
2024-10-11 17:19:54 -0500

    updates

--- new_scripts/unit_test/test_dcamera.py
+++ new_scripts/unit_test/test_dcamera.py
@@ -3,12 +3,12 @@ import numpy as np
 import cv2
 
 # Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
+pipeline = rs.pipeline() # type: ignore
+config = rs.config() # type: ignore
 
 # Enable depth and color streams
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30) # type: ignore
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30) # type: ignore
 
 # Start streaming
 pipeline.start(config)
