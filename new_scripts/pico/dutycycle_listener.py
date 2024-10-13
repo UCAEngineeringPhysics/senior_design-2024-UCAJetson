45b6ad3d43b2f544de72b70e306f7a04e15e4141
jabbott2 <jabbott2@cub.uca.edu>
2024-10-13 15:19:37 -0500

    add led light

--- new_scripts/pico/dutycycle_listener.py
+++ new_scripts/pico/dutycycle_listener.py
@@ -12,20 +12,28 @@ steering = PWM(Pin(0))
 steering.freq(50)
 throttle = PWM(Pin(15))
 throttle.freq(50)
-sleep(3)  # ESC calibrate
-poller = select.poll()
-poller.register(sys.stdin, select.POLLIN)
-event = poller.poll()
+led = Pin('LED')
+
 
 # LOOP
-while True:
-    # read data from serial
-    for msg, _ in event:
-        buffer = msg.readline().rstrip().split(',')
-        # print(buffer) # debug
-        # print(len(buffer)) # debug
-        if len(buffer) == 2:
-            ns_st, ns_th = int(buffer[0]), int(buffer[1])
-            print(ns_st, ns_th) # debug
-            steering.duty_ns(ns_st)
-            throttle.duty_ns(ns_th)
+try:
+    led.toggle()
+    sleep(3)  # ESC calibrate
+    poller = select.poll()
+    poller.register(sys.stdin, select.POLLIN)
+    event = poller.poll()
+
+    while True:
+        # read data from serial
+        
+        for msg, _ in event:
+            buffer = msg.readline().rstrip().split(',')
+            # print(buffer) # debug
+            # print(len(buffer)) # debug
+            if len(buffer) == 2:
+                ns_st, ns_th = int(buffer[0]), int(buffer[1])
+                print(ns_st, ns_th) # debug
+                steering.duty_ns(ns_st)
+                throttle.duty_ns(ns_th)
+except:
+    led.toggle()
